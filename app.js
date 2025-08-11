857bebffaa81ac3a7cefd8fbee8dfcd2ee7a8505
Hellfunny <alejandro.abinadi@hotmail.com>
2025-08-10 21:48:29 -0600

    Se concluyo y se agrego el README

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//cOMENZANDO POR INTRODUCIR VARIABLES
+
+let amigos = [];
+
+function agregarAmigo() {
+    amigos.push(document.getElementById('amigo').value);
+    actualizarListaAmigos();
+    document.getElementById('amigo').value = "";
+    return;
+}
+
+function actualizarListaAmigos() {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+    for (i=0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaHTML.appendChild(li);  
+    }
+    return;
+}
+
+function sortearAmigo() {
+    let textoHTML = document.getElementById('resultado');
+    let Resultado = 'No hay amigos';
+    
+    if(amigos.length != 0){
+        Resultado = amigos[Math.floor(Math.random()*amigos.length)];
+        textoHTML.innerHTML = Resultado;
+    } else {
+        alert('Ingrese un nombre valido');
+    }
+    return;
+}
